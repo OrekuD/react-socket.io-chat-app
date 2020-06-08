04ad558b4abde89953f4455d8f45dc56d7bd21fd
unknown <opokudavid446@gmail.com>
2020-06-08 21:00:21 +0100

    Add animations

--- frontend/src/components/Container.js
+++ frontend/src/components/Container.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import gsap from "gsap";
 
-const Container = ({ children }) => (
-  <div className="container">
-    <div className="circle-1" />
-    <div className="content">{children}</div>
-  </div>
-);
+const Container = ({ children }) => {
+  const animation = () => {
+    const tl = gsap.timeline();
+    tl.to(".circle-1", {
+      duration: 4,
+      top: Math.floor(Math.random() * window.innerHeight),
+      left: Math.floor(Math.random() * window.innerWidth),
+      repeat: true,
+      yoyo: true,
+      repeatDelay: 0,
+      onComplete: animation,
+    }).to(".circle-2", {
+      delay: -2,
+      duration: 4,
+      top: Math.floor(Math.random() * window.innerHeight),
+      left: Math.floor(Math.random() * window.innerWidth),
+      repeat: true,
+      yoyo: true,
+      repeatDelay: 0,
+      onComplete: animation,
+    });
+  };
+
+  useEffect(() => {
+    // animation();
+  }, []);
+
+  return (
+    <div className="container">
+      {/* <div className="circle-1" />
+      <div className="circle-2" /> */}
+      <div className="content">{children}</div>
+    </div>
+  );
+};
 export default Container;
