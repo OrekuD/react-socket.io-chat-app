8420e17fe81608ba40d181d5dc54c5d75c93bff0
unknown <opokudavid446@gmail.com>
2020-06-26 10:09:45 +0100

    Fixed bug

--- frontend/src/components/Container.js
+++ frontend/src/components/Container.js
@@ -2,10 +2,6 @@ import React from "react";
 
 const Container = ({ children }) => {
   const height = window.innerHeight;
-  return (
-    <div className="container" style={{ height }}>
-      {children}
-    </div>
-  );
+  return <div className="container">{children}</div>;
 };
 export default Container;
